3324e563c9dad3d8beaccf78f35ff72e40d1e3cd
fhellipevalentin <fhellipereis@outlook.com>
2021-03-20 17:31:51 -0300

    About View

--- Library/src/application/Main.java
+++ Library/src/application/Main.java
@@ -5,22 +5,24 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			ScrollPane scrollpane = loader.load();
+			ScrollPane scrollPane = loader.load();
 			
-			scrollpane.setFitToHeight(true);
-			scrollpane.setFitToWidth(true);
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollpane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Library");
 			primaryStage.show();
@@ -28,7 +30,11 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
