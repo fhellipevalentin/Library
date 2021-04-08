adf81be5092457b954956d017194b28a4c1f2bcd
fhellipevalentin <fhellipereis@outlook.com>
2021-04-08 16:44:39 -0300

    DepartmentDaoJDBC implementation with findById method

--- Library/src/model/dao/DaoFactory.java
+++ Library/src/model/dao/DaoFactory.java
@@ -2,10 +2,14 @@ package model.dao;
 
 import db.DB;
 import model.dao.Impl.BooksDaoJDBC;
+import model.dao.Impl.DepartmentDaoJDBC;
 
 public class DaoFactory {
 	
 	public static BooksDao createBooksDao() {
 		return new BooksDaoJDBC(DB.getConnection());
+	}
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
